e1bd1756a1da4c71899b3ebd1634513f627f102a
Ahmed Hany <ahmed_hany19@outlook.com>
2022-09-09 18:39:39 +0200

    Basic Setup

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My feed</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
